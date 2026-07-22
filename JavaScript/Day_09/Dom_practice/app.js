a17ee37880249ccc1bbd66e6a2adb042abe4306e
Anantsingh005 <anantsingha411@gmail.com>
2026-07-22 02:50:21 -0700

    Day ---> 09

--- JavaScript/Day_09/Dom_practice/app.js
+++ JavaScript/Day_09/Dom_practice/app.js
@@ -2,3 +2,13 @@ let smallImg =document.getElementsByClassName("oldImg");
 for (let i = 0; i < smallImg.length; i++) {
     console.dir(smallImg[i]);
 }
+
+let co =document.querySelectorAll(".box a");
+for(link of co){
+    link.style.color = "red";
+}
+
+
+// for (let i = 0; i < co.length; i++) {
+//     co[i].style.color = "red";
+// }
