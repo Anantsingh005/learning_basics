762a3db6cf678d11bc1b5427f8ef05c53f362d9d
Anant Singh <anantsingha411@gmail.com>
2026-08-28 19:16:44 +0530

    Day 1

--- Backend/Node.Js/Script.js
+++ Backend/Node.Js/Script.js
@@ -8,7 +8,21 @@
 
 // console.log(process.argv);
 
-let arg = process.argv;
-for(let i=2; i<arg.length; i++){
-    console.log("Hello to ", arg[i]);
-}
+// let arg = process.argv;
+// for(let i=2; i<arg.length; i++){
+//     console.log("Hello to ", arg[i]);
+// }
+
+// const math = require('./math');
+// console.log(math.G);
+// console.log(math.sum(5, 6));
+
+// const info = require("./fruits");
+// console.log(info);
+
+import{ mul, PI}from './math.js';
+import randomWord from 'random-word';
+randomWord();
+randomWord();
+console.log(mul(5, 6));
+console.log(PI);
---
+++ Backend/Node.Js/math.js
@@ -0,0 +1,21 @@
+// module.exports.sum = (a, b) => {a +b};
+// module.exports.mul = (a, b) => a*b;
+// module.exports.G = 9.8;
+// module.exports.PI = 3.14;
+
+
+// // let obj ={
+// //     sum: sum,
+// //     mul: mul,
+// //     G: g,
+// //     PI: pi
+// // }
+
+
+// // module.exports = obj;
+
+
+export const sum = (a, b) => a+b;
+export const mul = (a, b) => a * b;
+export const G = 9.8;
+export const PI = 3.14;
